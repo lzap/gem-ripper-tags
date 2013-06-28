7e653b9717d5fc5e9f89298712b75b126952f7da
Lukas Zapletal <lzap+git@redhat.com>
2013-06-28 11:08:28 +0200

    adding release script

---
+++ release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Before running this make sure you tagged and pushed!
+#
+
+VERSION=$(git describe --abbrev=0 --tags)
+git checkout $VERSION && \
+gem build gem-ripper-tags-$VERSION.gem && \
+gem push gem-ripper-tags-$VERSION.gem && \
